db91e5bd75318f34e8e2432c05f2136453e4c268
Joel Kaasinen <joel.kaasinen@metosin.fi>
2021-08-19 11:49:09 +0300

    release 0.1.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject com.github.opqdonut/clj-github-actions-example "0.1.0-SNAPSHOT"
-  :description "TODO"
+(defproject com.github.opqdonut/clj-github-actions-example "0.1.0"
+  :description "An example of testing & deploying a Clojure library using Github Actions. See Github repo."
   :url "http://github.com/opqdonut/clj-github-actions-example"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
@@ -8,5 +8,6 @@
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns clj-github-actions-example.core}
   :deploy-repositories [["clojars" {:url "https://repo.clojars.org/"
+                                    :sign-releases false
                                     :username :env/CLOJARS_USER
                                     :password :env/CLOJARS_DEPLOY_TOKEN}]])
