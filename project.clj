d9f57189c662f5d4191d6051bb2c57584562cca0
Joel Kaasinen <joel.kaasinen@metosin.fi>
2021-08-19 11:49:24 +0300

    bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.opqdonut/clj-github-actions-example "0.1.0"
+(defproject com.github.opqdonut/clj-github-actions-example "0.1.1-SNAPSHOT"
   :description "An example of testing & deploying a Clojure library using Github Actions. See Github repo."
   :url "http://github.com/opqdonut/clj-github-actions-example"
   :license {:name "Eclipse Public License"
